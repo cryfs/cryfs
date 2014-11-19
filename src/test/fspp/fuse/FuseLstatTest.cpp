36c0ed234800a446a5f8fef29f084396e0e896dc
Sebastian Messmer <mail@smessmer.de>
2014-11-19 17:12:47 +0100

    Added more lstat tests

--- src/test/fspp/fuse/FuseLstatTest.cpp
+++ src/test/fspp/fuse/FuseLstatTest.cpp
@@ -16,6 +16,10 @@ using std::string;
 
 class FuseLstatTest: public FuseTest {
 public:
+  const char *FILENAME = "/myfile";
+  const mode_t MODE1 = S_IFREG | S_IRUSR | S_IWGRP | S_IXOTH;
+  const mode_t MODE2 = S_IFDIR | S_IWUSR | S_IXGRP | S_IROTH;
+
   struct stat LstatPath(const string &path) {
     auto fs = TestFS();
 
@@ -55,4 +59,22 @@ TEST_F(FuseLstatTest, PathParameterIsCorrectNestedDir) {
   ReturnIsDirOnLstat("/mydir/mydir2");
   EXPECT_CALL(fsimpl, lstat(StrEq("/mydir/mydir2/mydir3"), _)).Times(1);
   LstatPath("/mydir/mydir2/mydir3/");
+}
+
+TEST_F(FuseLstatTest, ReturnedModeIsCorrect1) {
+  EXPECT_CALL(fsimpl, lstat(StrEq(FILENAME), _)).WillRepeatedly(Invoke([this](const char*, struct ::stat *stat) {
+    stat->st_mode = MODE1;
+  }));
+
+  struct ::stat result = LstatPath(FILENAME);
+  EXPECT_EQ(MODE1, result.st_mode);
+}
+
+TEST_F(FuseLstatTest, ReturnedModeIsCorrect2) {
+  EXPECT_CALL(fsimpl, lstat(StrEq(FILENAME), _)).WillRepeatedly(Invoke([this](const char*, struct ::stat *stat) {
+    stat->st_mode = MODE2;
+  }));
+
+  struct ::stat result = LstatPath(FILENAME);
+  EXPECT_EQ(MODE2, result.st_mode);
 }
