47a35c94b4257ddb288626738100aa02fe220eae
Sebastian Messmer <mail@smessmer.de>
2014-11-27 16:40:22 +0100

    Added write() test cases

--- src/test/testutils/VirtualTestFile.h
+++ src/test/testutils/VirtualTestFile.h
@@ -6,28 +6,42 @@
 
 class VirtualTestFile {
 public:
-  VirtualTestFile(size_t size);
+  VirtualTestFile(size_t size, long long int IV = 1);
   virtual ~VirtualTestFile();
 
   int read(void *buf, size_t count, off_t offset);
 
   // Return true, iff the given data is equal to the data of the file at the given offset.
-  bool fileContentEqual(char *content, size_t count, off_t offset);
+  bool fileContentEqual(const char *content, size_t count, off_t offset);
+
+  const char *data();
+
+  size_t size();
 
 protected:
   char *_fileData;
   size_t _size;
 
 private:
-  void fillFileWithRandomData();
+  void fillFileWithRandomData(long long int IV);
 };
 
 class VirtualTestFileWriteable: public VirtualTestFile {
 public:
-  VirtualTestFileWriteable(size_t size);
+  VirtualTestFileWriteable(size_t size, long long int IV = 1);
   virtual ~VirtualTestFileWriteable();
+
+  void write(const void *buf, size_t count, off_t offset);
+
+  bool sizeUnchanged();
+  bool regionUnchanged(off_t offset, size_t count);
+
 private:
-  char *originalFileData;
+  void extendFileSizeIfNecessary(size_t size);
+  void extendFileSize(size_t size);
+
+  char *_originalFileData;
+  size_t _originalSize;
 };
 
 #endif
