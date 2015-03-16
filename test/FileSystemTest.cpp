662c3f8e904b6426f39edc02c7cb132f6032748c
Sebastian Meßmer <mail@smessmer.de>
2015-03-16 02:15:51 +0100

    Use the fspp test cases for cryfs

---
+++ test/FileSystemTest.cpp
@@ -0,0 +1,31 @@
+#include <messmer/blockstore/implementations/testfake/FakeBlockStore.h>
+#include <messmer/fspp/fstest/FsTest.h>
+#include <messmer/tempfile/src/TempFile.h>
+
+#include "../src/CryDevice.h"
+
+using std::unique_ptr;
+using std::make_unique;
+
+using fspp::Device;
+
+using blockstore::testfake::FakeBlockStore;
+
+using namespace cryfs;
+
+class CryFsTestFixture: public FileSystemTestFixture {
+public:
+  CryFsTestFixture()
+  // Don't create config tempfile yet
+  : configFile(false) {}
+
+  unique_ptr<Device> createDevice() override {
+	auto blockStore = make_unique<FakeBlockStore>();
+	auto config = make_unique<CryConfig>(configFile.path());
+    return make_unique<CryDevice>(std::move(config), std::move(blockStore));
+  }
+
+  tempfile::TempFile configFile;
+};
+
+INSTANTIATE_TYPED_TEST_CASE_P(CryFS, FileSystemTest, CryFsTestFixture);
