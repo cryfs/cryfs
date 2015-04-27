cf6a8079b48a031f1985c7760c1bea0dcae67558
Sebastian Messmer <mail@smessmer.de>
2015-04-27 22:13:28 +0200

    TODOs

--- pointer.h
+++ pointer.h
@@ -9,6 +9,7 @@ namespace cpputils {
 /**
  * dynamic_cast implementation for unique_ptr (moving unique_ptr into a unique_ptr of different type)
  */
+//TODO Also allow passing a rvalue reference, otherwise dynamic_pointer_move(func()) won't work
 template<typename DST, typename SRC>
 inline std::unique_ptr<DST> dynamic_pointer_move(std::unique_ptr<SRC> &source) {
   //TODO Deleter
