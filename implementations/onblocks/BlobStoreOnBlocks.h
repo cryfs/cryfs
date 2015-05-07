864169ff0df1a2d8f7292ae46f8ce3b9697e42ea
Sebastian Messmer <mail@smessmer.de>
2015-05-08 01:57:44 +0200

    TODOs

--- implementations/onblocks/BlobStoreOnBlocks.h
+++ implementations/onblocks/BlobStoreOnBlocks.h
@@ -11,6 +11,8 @@ namespace parallelaccessdatatreestore {
 class ParallelAccessDataTreeStore;
 }
 
+//TODO Make blobstore able to cope with incomplete data (some blocks missing, because they're not synchronized yet) and write test cases for that
+
 class BlobStoreOnBlocks: public BlobStore {
 public:
   BlobStoreOnBlocks(std::unique_ptr<blockstore::BlockStore> blockStore, uint32_t blocksizeBytes);
