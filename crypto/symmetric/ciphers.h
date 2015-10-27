c50371d583e2a2a26d8ea9dfe44621c7f7021e68
Sebastian Messmer <mail@smessmer.de>
2015-10-27 23:19:20 +0100

    Add symmetric ciphers to crypto lib

---
+++ crypto/symmetric/ciphers.h
@@ -0,0 +1,47 @@
+#pragma once
+#ifndef MESSMER_CPPUTILS_CRYPTO_SYMMETRIC_CIPHERS_H_
+#define MESSMER_CPPUTILS_CRYPTO_SYMMETRIC_CIPHERS_H_
+
+#include <cryptopp/cryptopp/aes.h>
+#include <cryptopp/cryptopp/twofish.h>
+#include <cryptopp/cryptopp/serpent.h>
+#include <cryptopp/cryptopp/cast.h>
+#include <cryptopp/cryptopp/mars.h>
+#include "GCM_Cipher.h"
+#include "CFB_Cipher.h"
+
+namespace cpputils {
+
+static_assert(32 == CryptoPP::AES::MAX_KEYLENGTH, "If AES offered larger keys, we should offer a variant with it");
+using AES256_GCM = GCM_Cipher<CryptoPP::AES, 32>;
+using AES256_CFB = CFB_Cipher<CryptoPP::AES, 32>;
+using AES128_GCM = GCM_Cipher<CryptoPP::AES, 16>;
+using AES128_CFB = CFB_Cipher<CryptoPP::AES, 16>;
+
+static_assert(32 == CryptoPP::Twofish::MAX_KEYLENGTH, "If Twofish offered larger keys, we should offer a variant with it");
+using Twofish256_GCM = GCM_Cipher<CryptoPP::Twofish, 32>;
+using Twofish256_CFB = CFB_Cipher<CryptoPP::Twofish, 32>;
+using Twofish128_GCM = GCM_Cipher<CryptoPP::Twofish, 16>;
+using Twofish128_CFB = CFB_Cipher<CryptoPP::Twofish, 16>;
+
+static_assert(32 == CryptoPP::Serpent::MAX_KEYLENGTH, "If Serpent offered larger keys, we should offer a variant with it");
+using Serpent256_GCM = GCM_Cipher<CryptoPP::Serpent, 32>;
+using Serpent256_CFB = CFB_Cipher<CryptoPP::Serpent, 32>;
+using Serpent128_GCM = GCM_Cipher<CryptoPP::Serpent, 16>;
+using Serpent128_CFB = CFB_Cipher<CryptoPP::Serpent, 16>;
+
+static_assert(32 == CryptoPP::CAST256::MAX_KEYLENGTH, "If Cast offered larger keys, we should offer a variant with it");
+using Cast256_GCM = GCM_Cipher<CryptoPP::CAST256, 32>;
+using Cast256_CFB = CFB_Cipher<CryptoPP::CAST256, 32>;
+
+static_assert(56 == CryptoPP::MARS::MAX_KEYLENGTH, "If Mars offered larger keys, we should offer a variant with it");
+using Mars448_GCM = GCM_Cipher<CryptoPP::MARS, 56>;
+using Mars448_CFB = CFB_Cipher<CryptoPP::MARS, 56>;
+using Mars256_GCM = GCM_Cipher<CryptoPP::MARS, 32>;
+using Mars256_CFB = CFB_Cipher<CryptoPP::MARS, 32>;
+using Mars128_GCM = GCM_Cipher<CryptoPP::MARS, 16>;
+using Mars128_CFB = CFB_Cipher<CryptoPP::MARS, 16>;
+
+}
+
+#endif
---
+++ test/crypto/symmetric/CipherTest.cpp
@@ -0,0 +1,254 @@
+#include <google/gtest/gtest.h>
+#include "../../../crypto/symmetric/Cipher.h"
+#include "../../../crypto/symmetric/ciphers.h"
+#include "testutils/FakeAuthenticatedCipher.h"
+
+#include "../../../data/DataFixture.h"
+#include "../../../data/Data.h"
+#include <boost/optional/optional_io.hpp>
+
+using namespace cpputils;
+
+template<class Cipher>
+class CipherTest: public ::testing::Test {
+public:
+  BOOST_CONCEPT_ASSERT((CipherConcept<Cipher>));
+  typename Cipher::EncryptionKey encKey = createKeyFixture();
+
+  static typename Cipher::EncryptionKey createKeyFixture(int seed = 0) {
+    Data data = DataFixture::generate(Cipher::EncryptionKey::BINARY_LENGTH, seed);
+    return Cipher::EncryptionKey::FromBinary(data.data());
+  }
+
+  void CheckEncryptThenDecryptIsIdentity(const Data &plaintext) {
+    Data ciphertext = Encrypt(plaintext);
+    Data decrypted = Decrypt(ciphertext);
+    EXPECT_EQ(plaintext, decrypted);
+  }
+
+  void CheckEncryptIsIndeterministic(const Data &plaintext) {
+    Data ciphertext = Encrypt(plaintext);
+    Data ciphertext2 = Encrypt(plaintext);
+    EXPECT_NE(ciphertext, ciphertext2);
+  }
+
+  void CheckEncryptedSize(const Data &plaintext) {
+    Data ciphertext = Encrypt(plaintext);
+    EXPECT_EQ(Cipher::ciphertextSize(plaintext.size()), ciphertext.size());
+  }
+
+  void ExpectDoesntDecrypt(const Data &ciphertext) {
+    auto decrypted = Cipher::decrypt((byte*)ciphertext.data(), ciphertext.size(), this->encKey);
+    EXPECT_FALSE(decrypted);
+  }
+
+  Data Encrypt(const Data &plaintext) {
+    return Cipher::encrypt((byte*)plaintext.data(), plaintext.size(), this->encKey);
+  }
+
+  Data Decrypt(const Data &ciphertext) {
+    return Cipher::decrypt((byte*)ciphertext.data(), ciphertext.size(), this->encKey).value();
+  }
+
+  static Data CreateZeroes(unsigned int size) {
+    return std::move(Data(size).FillWithZeroes());
+  }
+
+  static Data CreateData(unsigned int size, unsigned int seed = 0) {
+    return DataFixture::generate(size, seed);
+  }
+};
+
+TYPED_TEST_CASE_P(CipherTest);
+
+constexpr std::initializer_list<unsigned int> SIZES = {0, 1, 100, 1024, 5000, 1048576, 20971520};
+
+TYPED_TEST_P(CipherTest, Size) {
+  for (auto size: SIZES) {
+    EXPECT_EQ(size, TypeParam::ciphertextSize(TypeParam::plaintextSize(size)));
+    EXPECT_EQ(size, TypeParam::plaintextSize(TypeParam::ciphertextSize(size)));
+  }
+}
+
+TYPED_TEST_P(CipherTest, EncryptThenDecrypt_Zeroes) {
+  for (auto size: SIZES) {
+    Data plaintext = this->CreateZeroes(size);
+    this->CheckEncryptThenDecryptIsIdentity(plaintext);
+  }
+}
+
+TYPED_TEST_P(CipherTest, EncryptThenDecrypt_Data) {
+  for (auto size: SIZES) {
+    Data plaintext = this->CreateData(size);
+    this->CheckEncryptThenDecryptIsIdentity(plaintext);
+  }
+}
+
+TYPED_TEST_P(CipherTest, EncryptIsIndeterministic_Zeroes) {
+  for (auto size: SIZES) {
+    Data plaintext = this->CreateZeroes(size);
+    this->CheckEncryptIsIndeterministic(plaintext);
+  }
+}
+
+TYPED_TEST_P(CipherTest, EncryptIsIndeterministic_Data) {
+  for (auto size: SIZES) {
+    Data plaintext = this->CreateData(size);
+    this->CheckEncryptIsIndeterministic(plaintext);
+  }
+}
+
+TYPED_TEST_P(CipherTest, EncryptedSize) {
+  for (auto size: SIZES) {
+    Data plaintext = this->CreateData(size);
+    this->CheckEncryptedSize(plaintext);
+  }
+}
+
+TYPED_TEST_P(CipherTest, TryDecryptDataThatIsTooSmall) {
+  Data tooSmallCiphertext(TypeParam::ciphertextSize(0) - 1);
+  this->ExpectDoesntDecrypt(tooSmallCiphertext);
+}
+
+TYPED_TEST_P(CipherTest, TryDecryptDataThatIsMuchTooSmall_0) {
+  static_assert(TypeParam::ciphertextSize(0) > 0, "If this fails, the test case doesn't make sense.");
+  Data tooSmallCiphertext(0);
+  this->ExpectDoesntDecrypt(tooSmallCiphertext);
+}
+
+TYPED_TEST_P(CipherTest, TryDecryptDataThatIsMuchTooSmall_1) {
+  static_assert(TypeParam::ciphertextSize(0) > 1, "If this fails, the test case doesn't make sense.");
+  Data tooSmallCiphertext(1);
+  this->ExpectDoesntDecrypt(tooSmallCiphertext);
+}
+
+REGISTER_TYPED_TEST_CASE_P(CipherTest,
+    Size,
+    EncryptThenDecrypt_Zeroes,
+    EncryptThenDecrypt_Data,
+    EncryptIsIndeterministic_Zeroes,
+    EncryptIsIndeterministic_Data,
+    EncryptedSize,
+    TryDecryptDataThatIsTooSmall,
+    TryDecryptDataThatIsMuchTooSmall_0,
+    TryDecryptDataThatIsMuchTooSmall_1
+);
+
+template<class Cipher>
+class AuthenticatedCipherTest: public CipherTest<Cipher> {
+public:
+  Data zeroes1 = CipherTest<Cipher>::CreateZeroes(1);
+  Data plaintext1 = CipherTest<Cipher>::CreateData(1);
+  Data zeroes2 = CipherTest<Cipher>::CreateZeroes(100 * 1024);
+  Data plaintext2 = CipherTest<Cipher>::CreateData(100 * 1024);
+};
+
+TYPED_TEST_CASE_P(AuthenticatedCipherTest);
+
+TYPED_TEST_P(AuthenticatedCipherTest, ModifyFirstByte_Zeroes_Size1) {
+  Data ciphertext = this->Encrypt(this->zeroes1);
+  *(byte*)ciphertext.data() = *(byte*)ciphertext.data() + 1;
+  this->ExpectDoesntDecrypt(ciphertext);
+}
+
+TYPED_TEST_P(AuthenticatedCipherTest, ModifyFirstByte_Data_Size1) {
+  Data ciphertext = this->Encrypt(this->plaintext1);
+  *(byte*)ciphertext.data() = *(byte*)ciphertext.data() + 1;
+  this->ExpectDoesntDecrypt(ciphertext);
+}
+
+TYPED_TEST_P(AuthenticatedCipherTest, ModifyFirstByte_Zeroes) {
+  Data ciphertext = this->Encrypt(this->zeroes2);
+  *(byte*)ciphertext.data() = *(byte*)ciphertext.data() + 1;
+  this->ExpectDoesntDecrypt(ciphertext);
+}
+
+TYPED_TEST_P(AuthenticatedCipherTest, ModifyFirstByte_Data) {
+  Data ciphertext = this->Encrypt(this->plaintext2);
+  *(byte*)ciphertext.data() = *(byte*)ciphertext.data() + 1;
+  this->ExpectDoesntDecrypt(ciphertext);
+}
+
+TYPED_TEST_P(AuthenticatedCipherTest, ModifyLastByte_Zeroes) {
+  Data ciphertext = this->Encrypt(this->zeroes2);
+  ((byte*)ciphertext.data())[ciphertext.size() - 1] = ((byte*)ciphertext.data())[ciphertext.size() - 1] + 1;
+  this->ExpectDoesntDecrypt(ciphertext);
+}
+
+TYPED_TEST_P(AuthenticatedCipherTest, ModifyLastByte_Data) {
+  Data ciphertext = this->Encrypt(this->plaintext2);
+  ((byte*)ciphertext.data())[ciphertext.size() - 1] = ((byte*)ciphertext.data())[ciphertext.size() - 1] + 1;
+  this->ExpectDoesntDecrypt(ciphertext);
+}
+
+TYPED_TEST_P(AuthenticatedCipherTest, ModifyMiddleByte_Zeroes) {
+  Data ciphertext = this->Encrypt(this->zeroes2);
+  ((byte*)ciphertext.data())[ciphertext.size()/2] = ((byte*)ciphertext.data())[ciphertext.size()/2] + 1;
+  this->ExpectDoesntDecrypt(ciphertext);
+}
+
+TYPED_TEST_P(AuthenticatedCipherTest, ModifyMiddleByte_Data) {
+  Data ciphertext = this->Encrypt(this->plaintext2);
+  ((byte*)ciphertext.data())[ciphertext.size()/2] = ((byte*)ciphertext.data())[ciphertext.size()/2] + 1;
+  this->ExpectDoesntDecrypt(ciphertext);
+}
+
+TYPED_TEST_P(AuthenticatedCipherTest, TryDecryptZeroesData) {
+  this->ExpectDoesntDecrypt(this->zeroes2);
+}
+
+TYPED_TEST_P(AuthenticatedCipherTest, TryDecryptRandomData) {
+  this->ExpectDoesntDecrypt(this->plaintext2);
+}
+
+REGISTER_TYPED_TEST_CASE_P(AuthenticatedCipherTest,
+  ModifyFirstByte_Zeroes_Size1,
+  ModifyFirstByte_Zeroes,
+  ModifyFirstByte_Data_Size1,
+  ModifyFirstByte_Data,
+  ModifyLastByte_Zeroes,
+  ModifyLastByte_Data,
+  ModifyMiddleByte_Zeroes,
+  ModifyMiddleByte_Data,
+  TryDecryptZeroesData,
+  TryDecryptRandomData
+);
+
+
+INSTANTIATE_TYPED_TEST_CASE_P(Fake, CipherTest, FakeAuthenticatedCipher);
+INSTANTIATE_TYPED_TEST_CASE_P(Fake, AuthenticatedCipherTest, FakeAuthenticatedCipher);
+
+INSTANTIATE_TYPED_TEST_CASE_P(AES256_CFB, CipherTest, AES256_CFB); //CFB mode is not authenticated
+INSTANTIATE_TYPED_TEST_CASE_P(AES256_GCM, CipherTest, AES256_GCM);
+INSTANTIATE_TYPED_TEST_CASE_P(AES256_GCM, AuthenticatedCipherTest, AES256_GCM);
+INSTANTIATE_TYPED_TEST_CASE_P(AES128_CFB, CipherTest, AES128_CFB); //CFB mode is not authenticated
+INSTANTIATE_TYPED_TEST_CASE_P(AES128_GCM, CipherTest, AES128_GCM);
+INSTANTIATE_TYPED_TEST_CASE_P(AES128_GCM, AuthenticatedCipherTest, AES128_GCM);
+
+INSTANTIATE_TYPED_TEST_CASE_P(Twofish256_CFB, CipherTest, Twofish256_CFB); //CFB mode is not authenticated
+INSTANTIATE_TYPED_TEST_CASE_P(Twofish256_GCM, CipherTest, Twofish256_GCM);
+INSTANTIATE_TYPED_TEST_CASE_P(Twofish256_GCM, AuthenticatedCipherTest, Twofish256_GCM);
+INSTANTIATE_TYPED_TEST_CASE_P(Twofish128_CFB, CipherTest, Twofish128_CFB); //CFB mode is not authenticated
+INSTANTIATE_TYPED_TEST_CASE_P(Twofish128_GCM, CipherTest, Twofish128_GCM);
+INSTANTIATE_TYPED_TEST_CASE_P(Twofish128_GCM, AuthenticatedCipherTest, Twofish128_GCM);
+
+INSTANTIATE_TYPED_TEST_CASE_P(Serpent256_CFB, CipherTest, Serpent256_CFB); //CFB mode is not authenticated
+INSTANTIATE_TYPED_TEST_CASE_P(Serpent256_GCM, CipherTest, Serpent256_GCM);
+INSTANTIATE_TYPED_TEST_CASE_P(Serpent256_GCM, AuthenticatedCipherTest, Serpent256_GCM);
+INSTANTIATE_TYPED_TEST_CASE_P(Serpent128_CFB, CipherTest, Serpent128_CFB); //CFB mode is not authenticated
+INSTANTIATE_TYPED_TEST_CASE_P(Serpent128_GCM, CipherTest, Serpent128_GCM);
+INSTANTIATE_TYPED_TEST_CASE_P(Serpent128_GCM, AuthenticatedCipherTest, Serpent128_GCM);
+
+INSTANTIATE_TYPED_TEST_CASE_P(Cast256_CFB, CipherTest, Cast256_CFB); //CFB mode is not authenticated
+INSTANTIATE_TYPED_TEST_CASE_P(Cast256_GCM, CipherTest, Cast256_GCM);
+INSTANTIATE_TYPED_TEST_CASE_P(Cast256_GCM, AuthenticatedCipherTest, Cast256_GCM);
+
+INSTANTIATE_TYPED_TEST_CASE_P(Mars448_CFB, CipherTest, Mars448_CFB); //CFB mode is not authenticated
+INSTANTIATE_TYPED_TEST_CASE_P(Mars448_GCM, CipherTest, Mars448_GCM);
+INSTANTIATE_TYPED_TEST_CASE_P(Mars448_GCM, AuthenticatedCipherTest, Mars448_GCM);
+INSTANTIATE_TYPED_TEST_CASE_P(Mars256_CFB, CipherTest, Mars256_CFB); //CFB mode is not authenticated
+INSTANTIATE_TYPED_TEST_CASE_P(Mars256_GCM, CipherTest, Mars256_GCM);
+INSTANTIATE_TYPED_TEST_CASE_P(Mars256_GCM, AuthenticatedCipherTest, Mars256_GCM);
+INSTANTIATE_TYPED_TEST_CASE_P(Mars128_CFB, CipherTest, Mars128_CFB); //CFB mode is not authenticated
+INSTANTIATE_TYPED_TEST_CASE_P(Mars128_GCM, CipherTest, Mars128_GCM);
+INSTANTIATE_TYPED_TEST_CASE_P(Mars128_GCM, AuthenticatedCipherTest, Mars128_GCM);
