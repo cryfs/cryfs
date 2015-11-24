68675c6212879408a3625102cb6fd15bf4e63512
Sebastian Messmer <messmer@cryfs.org>
2015-11-24 08:08:11 +0100

    - Move HttpClient to cpputils - Fix VersionChecker behaviour when the returned json is invalid - Add test cases for VersionChecker

--- src/cli/VersionChecker.cpp
+++ src/cli/VersionChecker.cpp
@@ -1,6 +1,6 @@
 #include "VersionChecker.h"
 #include <sstream>
-#include "HttpClient.h"
+#include <messmer/cpp-utils/network/CurlHttpClient.h>
 #include <boost/property_tree/json_parser.hpp>
 #include <messmer/cpp-utils/logging/logging.h>
 #include <boost/foreach.hpp>
@@ -8,14 +8,22 @@
 using boost::optional;
 using boost::none;
 using std::string;
+using std::shared_ptr;
+using std::make_shared;
+using cpputils::HttpClient;
+using cpputils::CurlHttpClient;
 using boost::property_tree::ptree;
 using boost::property_tree::json_parser_error;
 using namespace cpputils::logging;
 
-#include <iostream>
 namespace cryfs {
 
-    VersionChecker::VersionChecker(): _versionInfo(_getVersionInfo()) {
+    VersionChecker::VersionChecker()
+            :VersionChecker(make_shared<CurlHttpClient>()) {
+    }
+
+    VersionChecker::VersionChecker(shared_ptr<HttpClient> httpClient)
+            : _versionInfo(_getVersionInfo(std::move(httpClient))) {
     }
 
     optional<string> VersionChecker::newestVersion() const {
@@ -33,7 +41,11 @@ namespace cryfs {
         if (_versionInfo == none) {
             return none;
         }
-        BOOST_FOREACH(const ptree::value_type &v, _versionInfo->get_child("warnings")) {
+        auto warnings = _versionInfo->get_child_optional("warnings");
+        if (warnings == none) {
+            return none;
+        }
+        BOOST_FOREACH(const ptree::value_type &v, *warnings) {
             if(v.first == version) {
                 return v.second.get_value<std::string>();
             }
@@ -41,10 +53,9 @@ namespace cryfs {
         return none;
     }
 
-    optional<ptree> VersionChecker::_getVersionInfo() {
-        optional<string> response = HttpClient().get("http://www.cryfs.org/version_info.json");
+    optional<ptree> VersionChecker::_getVersionInfo(shared_ptr<HttpClient> httpClient) {
+        optional<string> response = httpClient->get("http://www.cryfs.org/version_info.json");
         if (response == none) {
-            std::cout << "no response" << std::endl;
             return none;
         }
         return _parseJson(*response);
--- src/cli/VersionChecker.h
+++ src/cli/VersionChecker.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <boost/optional.hpp>
 #include <boost/property_tree/ptree.hpp>
+#include <messmer/cpp-utils/network/HttpClient.h>
 
 namespace cryfs {
     //TODO Test
@@ -12,11 +13,13 @@ namespace cryfs {
     class VersionChecker final {
     public:
         VersionChecker();
+        //TODO Write a cpputils::shared_ref and use it
+        VersionChecker(std::shared_ptr<cpputils::HttpClient> httpClient);
 
         boost::optional<std::string> newestVersion() const;
         boost::optional<std::string> securityWarningFor(const std::string &version) const;
     private:
-        static boost::optional<boost::property_tree::ptree> _getVersionInfo();
+        static boost::optional<boost::property_tree::ptree> _getVersionInfo(std::shared_ptr<cpputils::HttpClient> httpClient);
         static boost::optional<boost::property_tree::ptree> _parseJson(const std::string &json);
 
         boost::optional<boost::property_tree::ptree> _versionInfo;
