10ac4eef2931b7b93034ac8e29e86d44749920ab
Sebastian Messmer <messmer@cryfs.org>
2015-12-14 17:18:53 +0100

    Added DataUtils

---
+++ data/DataUtils.cpp
@@ -0,0 +1,12 @@
+#include "DataUtils.h"
+
+namespace cpputils {
+    namespace DataUtils {
+        Data resize(Data data, size_t newSize) {
+            Data newData(newSize);
+            newData.FillWithZeroes(); // TODO Only fill region after copied old data with zeroes
+            std::memcpy(newData.data(), data.data(), std::min(newData.size(), data.size()));
+            return newData;
+        }
+    }
+}
