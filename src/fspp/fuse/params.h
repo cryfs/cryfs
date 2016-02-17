014b5703e040cdbd58891d417010d9d5cb6cca5a
Sebastian Messmer <messmer@cryfs.org>
2016-02-17 22:30:28 +0100

    Instead of modifying include paths, include osxfuse header from the right location

--- src/fspp/fuse/params.h
+++ src/fspp/fuse/params.h
@@ -3,6 +3,12 @@
 #define MESSMER_FSPP_FUSE_PARAMS_H_
 
 #define FUSE_USE_VERSION 26
+#ifdef __linux__
 #include <fuse.h>
+#elif __APPLE__
+#include <osxfuse/fuse.h>
+#else
+#error System not supported
+#endif
 
 #endif
