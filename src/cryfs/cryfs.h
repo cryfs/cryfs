a764b1a12e90bcdc83b73f15eec1c0f6175f8123
Sebastian Messmer <mail@smessmer.de>
2016-02-25 22:05:24 +0100

    The library only exports the symbols that are defined to be exported

--- src/cryfs/cryfs.h
+++ src/cryfs/cryfs.h
@@ -1,6 +1,8 @@
 #pragma once
-#ifndef MESSMER_CRYFS_CRYFS_H
-#define MESSMER_CRYFS_CRYFS_H
+#ifndef CRYFS_CRYFS_H
+#define CRYFS_CRYFS_H
+
+#include "cryfs_export.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -9,9 +11,9 @@ extern "C" {
 typedef struct cryfs_load_context cryfs_load_context;
 typedef enum {cryfs_success = 0} cryfs_status;
 
-cryfs_status cryfs_load_init(cryfs_load_context **context) __attribute__((warn_unused_result));
-void cryfs_load_free(cryfs_load_context *context);
-const char *cryfs_test(cryfs_load_context *context) __attribute__((warn_unused_result));
+CRYFS_EXPORT __attribute__((warn_unused_result)) cryfs_status cryfs_load_init(cryfs_load_context **context);
+CRYFS_EXPORT __attribute__((warn_unused_result)) const char *cryfs_test(cryfs_load_context *context);
+CRYFS_EXPORT void cryfs_load_free(cryfs_load_context *context);
 
 #ifdef __cplusplus
 }
