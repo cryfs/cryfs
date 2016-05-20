01044539e239938dcda26526efbb5516fdbbfb44
Sebastian Messmer <messmer@cryfs.org>
2016-05-20 12:29:43 -0700

    Documentation for daemonize()

--- src/cpp-utils/process/daemon/daemonize.h
+++ src/cpp-utils/process/daemon/daemonize.h
@@ -8,6 +8,17 @@
 
 namespace cpputils {
     namespace process {
+        /**
+         * Fork a child process, set it up as a daemon process, and run childProgram inside it.
+         *
+         * The childProgram gets a PipeToParent, which it can use to notify the parent process
+         * when the daemon is ready, or if there was an error during initialization.
+         * After execution of the childProgram, the child process will exit and not return to the caller.
+         * That means for example that no destructors of outside allocated objects are called.
+         *
+         * In the parent process, daemonize() returns to the caller and returns a PipeFromChild,
+         * which can be used to wait for the ready signal from the child process.
+         */
         PipeFromChild daemonize(std::function<void (PipeToParent *)> childProgram);
     }
 }
