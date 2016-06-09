7c11189bb81165e240a1bd734a189274a95987cd
Sebastian Messmer <messmer@cryfs.org>
2016-06-08 22:15:30 -0700

    Add include needed for Mac OS X

--- src/cpp-utils/process/pipe/PipeDescriptor.cpp
+++ src/cpp-utils/process/pipe/PipeDescriptor.cpp
@@ -1,6 +1,7 @@
 #include "PipeDescriptor.h"
 #include "../../logging/logging.h"
 #include "../../assert/assert.h"
+#include <unistd.h>
 
 using namespace cpputils::process;
 using namespace cpputils::logging;
