a1c3079a684fa06d3d5e4fdd935cdc141a62cf99
Sebastian Messmer <mail@smessmer.de>
2016-09-24 09:51:29 +0200

    Catch exceptions from main

--- src/cryfs-cli/main.cpp
+++ src/cryfs-cli/main.cpp
@@ -8,9 +8,15 @@ using cpputils::Random;
 using cpputils::SCrypt;
 using cpputils::CurlHttpClient;
 using std::make_shared;
+using std::cerr;
 using cpputils::IOStreamConsole;
 
 int main(int argc, const char *argv[]) {
-    auto &keyGenerator = Random::OSRandom();
-    return Cli(keyGenerator, SCrypt::DefaultSettings, make_shared<IOStreamConsole>(), make_shared<CurlHttpClient>()).main(argc, argv);
+    try {
+        auto &keyGenerator = Random::OSRandom();
+        return Cli(keyGenerator, SCrypt::DefaultSettings, make_shared<IOStreamConsole>(),
+                   make_shared<CurlHttpClient>()).main(argc, argv);
+    } catch (const std::exception &e) {
+        cerr << "Error: " << e.what();
+    }
 }
