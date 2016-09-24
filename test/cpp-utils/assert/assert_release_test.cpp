378e259eee93ba0d6b7d3a92d24e1ef8442d996f
Sebastian Messmer <mail@smessmer.de>
2016-09-24 17:05:45 +0200

    Fix assert_release_test

--- test/cpp-utils/assert/assert_release_test.cpp
+++ test/cpp-utils/assert/assert_release_test.cpp
@@ -26,7 +26,7 @@ TEST(AssertTest_ReleaseBuild, AssertMessage) {
     FAIL();
   } catch (const cpputils::AssertFailed &e) {
     EXPECT_THAT(e.what(), MatchesRegex(
-        "Assertion \\[2==5\\] failed in .*/assert_release_test.cpp:23: my message.*"
+        "Assertion \\[2==5\\] failed in .*/assert_release_test.cpp:25: my message.*"
     ));
   }
 }
