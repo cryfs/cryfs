b8cd968a83f22094189f1909b0c9fa38574432dd
Gerrit Toehgiono <gerrittoehgiono@googlemail.com>
2017-09-28 16:28:46 +0200

    update 'defined'-style (src/spp/fuse/params.h)

--- src/fspp/fuse/params.h
+++ src/fspp/fuse/params.h
@@ -3,7 +3,7 @@
 #define MESSMER_FSPP_FUSE_PARAMS_H_
 
 #define FUSE_USE_VERSION 26
-#if defined __linux__ || defined __FreeBSD__
+#if defined(__linux__) || defined(__FreeBSD__)
 #include <fuse.h>
 #elif __APPLE__
 #include <osxfuse/fuse.h>
