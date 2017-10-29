67c912990943cb68cfb0e6547c8931ff6a04b0bf
Sebastian Messmer <messmer@cryfs.org>
2017-10-29 15:32:39 +0000

    Add script to explicitly run clang-tidy on the whole code base

---
+++ run-clang-tidy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Note: Call this from a cmake build directory (e.g. cmake/) for out-of-source builds
+# Examples:
+# mkdir cmake && cd cmake && ../run-clang-tidy.sh
+# mkdir cmake && cd cmake && ../run-clang-tidy.sh -fix
+# mkdir cmake && cd cmake && ../run-clang-tidy.sh -export-fixes fixes.yaml
+
+set -e
+
+NUMCORES=`nproc`
+
+# Run cmake in current working directory, but on source that is in the same directory as this script file
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON "${0%/*}"
+
+run-clang-tidy.py -j${NUMCORES} -quiet -header-filter "$(realpath ${0%/*})/(src|test)/.*" $@
+
