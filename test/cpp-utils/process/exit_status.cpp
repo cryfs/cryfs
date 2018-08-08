6f175e0b9b7d7c5efdfb8775f0094f7a0c53b948
Sebastian Messmer <messmer@cryfs.org>
2018-08-07 18:04:32 -0700

    Fix CI

--- test/cpp-utils/process/exit_status.cpp
+++ test/cpp-utils/process/exit_status.cpp
@@ -1,11 +1,12 @@
 // This is a small executable that prints its first argument and exits with the exit status in its second argument
 
 #include <iostream>
+#include <cstdlib>
 
 int main(int argc, char* argv[]) {
 	if (argc != 3) {
 		std::cerr << "Wrong number of arguments" << std::endl;
-		abort();
+		std::abort();
 	}
 
 	std::cout << argv[1];
