6761b233d747729787296cd26d966918e6bb816d
Sebastian Messmer <messmer@cryfs.org>
2018-09-03 07:43:44 -0700

    Fix FuseThread for Windows

--- test/fspp/testutils/FuseThread.cpp
+++ test/fspp/testutils/FuseThread.cpp
@@ -28,10 +28,24 @@ void FuseThread::start(const bf::path &mountDir, const vector<string> &fuseOptio
 #endif
 }
 
+namespace {
+#if !defined(_MSC_VER)
+void kill_thread(boost::thread* thread) {
+	if (0 != pthread_kill(thread->native_handle(), SIGINT)) {
+		throw std::runtime_error("Error sending stop signal");
+	}
+}
+#else
+void kill_thread(boost::thread* thread) {
+	if (0 == TerminateThread(thread->native_handle(), 0)) {
+		throw std::runtime_error("Error sending stop signal");
+	}
+}
+#endif
+}
+
 void FuseThread::stop() {
-  if (0 != pthread_kill(_child.native_handle(), SIGINT)) {
-      throw std::runtime_error("Error sending stop signal");
-  }
+  kill_thread(&_child);
   bool thread_stopped = _child.try_join_for(seconds(10));
   ASSERT(thread_stopped, "FuseThread could not be stopped");
   //Wait until it is properly shutdown (busy waiting is simple and doesn't hurt much here)
