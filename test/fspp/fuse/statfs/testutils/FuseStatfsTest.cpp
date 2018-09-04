8d10dba64b96ef7ea578976e340b5411ea7e1d8f
Sebastian Messmer <messmer@cryfs.org>
2018-09-03 21:11:12 -0700

    Fix statvfs (likely broken, see TODO)?

--- test/fspp/fuse/statfs/testutils/FuseStatfsTest.cpp
+++ test/fspp/fuse/statfs/testutils/FuseStatfsTest.cpp
@@ -20,11 +20,34 @@ void FuseStatfsTest::Statfs(const std::string &path, struct ::statvfs *result) {
   EXPECT_EQ(0, error) << "lstat syscall failed. errno: " << errno;
 }
 
+#if defined(_MSC_VER)
+#include <Windows.h>
+int call_statvfs(const char* filepath, struct ::statvfs * result) {
+	ULARGE_INTEGER freeBytesAvailableToCaller, totalNumberOfBytes, totalNumberOfFreeBytes;
+	BOOL success = GetDiskFreeSpaceExA(filepath, &freeBytesAvailableToCaller, &totalNumberOfBytes, &totalNumberOfFreeBytes);
+	if (!success) {
+		throw std::runtime_error("GetDiskFreeSpaceA failed with " + std::to_string(GetLastError()));
+	}
+	// Hack it together so that DokanY's size calculation does the right thing
+	// see https://github.com/dokan-dev/dokany/blob/224fc0880901d86ed98a04e355ee920fe6f095ef/dokan_fuse/src/fusemain.cpp#L947
+	// TODO This is likely not going to work because it's not dokan but our own code checking each value individually (e.g. FuseStatfsReturnBfreeTest)
+	result->f_bsize = 1;
+	result->f_bavail = freeBytesAvailableToCaller.QuadPart;
+	result->f_blocks = totalNumberOfBytes.QuadPart;
+	result->f_bfree = totalNumberOfFreeBytes.QuadPart;
+	return 0;
+}
+#else
+int call_statvfs(const char* filepath, struct :statvfs * result) {
+	return ::statvfs(filepath, result);
+}
+#endif
+
 int FuseStatfsTest::StatfsReturnError(const std::string &path, struct ::statvfs *result) {
   auto fs = TestFS();
 
   auto realpath = fs->mountDir() / path;
-  int retval = ::statvfs(realpath.string().c_str(), result);
+  int retval = call_statvfs(realpath.string().c_str(), result);
   if (retval == 0) {
     return 0;
   } else {
@@ -43,4 +66,3 @@ struct ::statvfs FuseStatfsTest::CallStatfsWithImpl(function<void(struct ::statv
 
   return result;
 }
-
