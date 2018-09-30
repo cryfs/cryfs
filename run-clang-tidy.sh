d91a7358c04a7d48a18b6ae3df88ae0c25965559
Sebastian Messmer <messmer@cryfs.org>
2018-09-29 17:18:30 -0700

    Fix run-clang-tidy.sh

--- run-clang-tidy.sh
+++ run-clang-tidy.sh
@@ -13,8 +13,4 @@ NUMCORES=`nproc`
 # Run cmake in current working directory, but on source that is in the same directory as this script file
 cmake -DBUILD_TESTING=on -DCMAKE_EXPORT_COMPILE_COMMANDS=ON "${0%/*}"
 
-# Build scrypt first. Our Makefiles call ino theirs, and this is needed to generate some header files. Clang-tidy will otherwise complain they're missing.
-make -j${NUMCORES} scrypt
-
 run-clang-tidy.py -j${NUMCORES} -quiet -header-filter "$(realpath ${0%/*})/(src|test)/.*" $@
-
