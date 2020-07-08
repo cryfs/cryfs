e2cd97de96ff470d83b734c7484269d420b1f705
Sebastian Messmer <messmer@cryfs.org>
2020-07-08 00:11:29 -0700

    Update ranges-v3 to 0.11.0

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake
 class CryFSConan(ConanFile):
 	settings = "os", "compiler", "build_type", "arch"
 	requires = [
-		"range-v3/0.9.1@ericniebler/stable",
+		"range-v3/0.11.0@ericniebler/stable",
 		"spdlog/1.8.2",
 	]
 	generators = "cmake"
