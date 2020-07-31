08d97884bd750b8ba7b972c4cc096666f7d66812
Jeremy Meadows <37280013+jeremymeadows@users.noreply.github.com>
2020-07-30 22:34:21 -0600

    fixed the bugs that clang-tidy caused
    never used clang before, so I don't really know what it did and why it
    caused compile errors

--- src/cryfs/impl/filesystem/cachingfsblobstore/DirBlobRef.h
+++ src/cryfs/impl/filesystem/cachingfsblobstore/DirBlobRef.h
@@ -98,11 +98,11 @@ public:
         return _base->AppendChildrenTo(result);
     }
 
-    const blockstore::BlockId &blockId() const override override {
+    const blockstore::BlockId &blockId() const override {
         return _base->blockId();
     }
 
-    fspp::num_bytes_t lstat_size() const override override {
+    fspp::num_bytes_t lstat_size() const override {
         return _base->lstat_size();
     }
 
