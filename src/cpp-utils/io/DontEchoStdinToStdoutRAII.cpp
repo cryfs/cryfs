c28cb2b2c471ef54a8568cecbb5436b805bcf967
Sebastian Messmer <messmer@cryfs.org>
2021-04-19 16:45:11 -0700

    Add Windows CI to Github Actions

--- src/cpp-utils/io/DontEchoStdinToStdoutRAII.cpp
+++ src/cpp-utils/io/DontEchoStdinToStdoutRAII.cpp
@@ -5,69 +5,80 @@
 #include <termios.h>
 #include <unistd.h>
 
-namespace cpputils {
-namespace details {
+namespace cpputils
+{
+    namespace details
+    {
 
-class DontEchoStdinToStdoutRAII final {
-public:
-    DontEchoStdinToStdoutRAII() : _old_state() {
-        tcgetattr(STDIN_FILENO, &_old_state);
-        termios new_state = _old_state;
-        new_state.c_lflag &= ~ECHO;
-        tcsetattr(STDIN_FILENO, TCSANOW, &new_state);
+        class DontEchoStdinToStdoutRAII_ final
+        {
+        public:
+            DontEchoStdinToStdoutRAII_() : _old_state()
+            {
+                tcgetattr(STDIN_FILENO, &_old_state);
+                termios new_state = _old_state;
+                new_state.c_lflag &= ~ECHO;
+                tcsetattr(STDIN_FILENO, TCSANOW, &new_state);
+            }
+
+            ~DontEchoStdinToStdoutRAII_()
+            {
+                tcsetattr(STDIN_FILENO, TCSANOW, &_old_state);
+            }
+
+        private:
+            termios _old_state;
+
+            DISALLOW_COPY_AND_ASSIGN(DontEchoStdinToStdoutRAII_);
+        };
+
     }
-
-    ~DontEchoStdinToStdoutRAII() {
-        tcsetattr(STDIN_FILENO, TCSANOW, &_old_state);
-    }
-
-private:
-    termios _old_state;
-
-    DISALLOW_COPY_AND_ASSIGN(DontEchoStdinToStdoutRAII);
-};
-
-}
 }
 
 #else
 
 #include <windows.h>
 
-namespace cpputils {
-namespace details {
+namespace cpputils
+{
+    namespace details
+    {
 
-class _DontEchoStdinToStdoutRAII final {
-public:
-	_DontEchoStdinToStdoutRAII() : _old_state() {
-		HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
-		GetConsoleMode(hStdin, &_old_state);
-		SetConsoleMode(hStdin, _old_state & (~ENABLE_ECHO_INPUT));
-	}
+        class DontEchoStdinToStdoutRAII_ final
+        {
+        public:
+            DontEchoStdinToStdoutRAII_() : _old_state()
+            {
+                HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+                GetConsoleMode(hStdin, &_old_state);
+                SetConsoleMode(hStdin, _old_state & (~ENABLE_ECHO_INPUT));
+            }
 
-	~_DontEchoStdinToStdoutRAII() {
-		HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
-		SetConsoleMode(hStdin, _old_state);
-	}
+            ~DontEchoStdinToStdoutRAII_()
+            {
+                HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+                SetConsoleMode(hStdin, _old_state);
+            }
 
-private:
-	DWORD _old_state;
+        private:
+            DWORD _old_state;
 
-	DISALLOW_COPY_AND_ASSIGN(_DontEchoStdinToStdoutRAII);
-};
+            DISALLOW_COPY_AND_ASSIGN(DontEchoStdinToStdoutRAII_);
+        };
 
-}
+    }
 }
 
 #endif
 
 using cpputils::make_unique_ref;
 
-namespace cpputils {
+namespace cpputils
+{
 
-DontEchoStdinToStdoutRAII::DontEchoStdinToStdoutRAII()
-    : raii(make_unique_ref<details::DontEchoStdinToStdoutRAII>()) {}
+    DontEchoStdinToStdoutRAII::DontEchoStdinToStdoutRAII()
+        : raii(make_unique_ref<details::DontEchoStdinToStdoutRAII_>()) {}
 
-DontEchoStdinToStdoutRAII::~DontEchoStdinToStdoutRAII() {}
+    DontEchoStdinToStdoutRAII::~DontEchoStdinToStdoutRAII() {}
 
 }
