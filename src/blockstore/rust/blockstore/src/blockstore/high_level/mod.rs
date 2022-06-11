dd7811d8aa849677d41a0311d5c7ab2a04ef3a9d
Sebastian Messmer <messmer@cryfs.org>
2022-06-11 04:22:56 -0700

    Add locking so that we don't open the same block multiple times or remove a block while it is open

--- src/blockstore/rust/blockstore/src/blockstore/high_level/mod.rs
+++ src/blockstore/rust/blockstore/src/blockstore/high_level/mod.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{bail, Result};
 use async_trait::async_trait;
 use futures::stream::Stream;
 use log;
@@ -6,18 +6,18 @@ use std::fmt;
 use std::future::Future;
 use std::pin::Pin;
 use std::sync::Arc;
+use lockpool::{AsyncLockPool, LockPool, TokioLockPool, TryLockError};
 
 use crate::blockstore::BlockId;
 use crate::data::Data;
 use crate::utils::async_drop::{AsyncDrop, AsyncDropGuard};
-
-// TODO Add locking so that we don't open the same block multiple times or remove a block while it is open
 
 pub struct Block<B: super::low_level::BlockStore + Send + Sync> {
     block_id: BlockId,
     data: Data,
     dirty: bool,
     base_store: Arc<B>,
+    lock_guard: <TokioLockPool<BlockId> as LockPool<BlockId>>::OwnedGuard,
 }
 
 impl<B: super::low_level::BlockStore + Send + Sync> fmt::Debug for Block<B> {
@@ -73,41 +73,60 @@ impl<B: super::low_level::BlockStore + Send + Sync> AsyncDrop for Block<B> {
 // TODO Should we require B: OptimizedBlockStoreWriter and use its methods?
 pub struct LockingBlockStore<B: super::low_level::BlockStore + Send + Sync> {
     base_store: Arc<B>,
+    lock_pool: Arc<TokioLockPool<BlockId>>,
 }
 
 impl<B: super::low_level::BlockStore + Send + Sync> LockingBlockStore<B> {
     pub fn new(base_store: B) -> Self {
         Self {
             base_store: Arc::new(base_store),
+            lock_pool: Arc::new(LockPool::new()),
         }
     }
 
     pub async fn load(&self, block_id: BlockId) -> Result<Option<AsyncDropGuard<Block<B>>>> {
+        let lock_guard = self.lock_pool.lock_owned_async(block_id).await;
         Ok(self.base_store.load(&block_id).await?.map(|data| {
             AsyncDropGuard::new(Block {
                 block_id,
                 data,
                 dirty: false,
                 base_store: Arc::clone(&self.base_store),
+                lock_guard,
             })
         }))
     }
 
     pub async fn try_create(&self, block_id: &BlockId, data: &Data) -> Result<TryCreateResult> {
-        // TODO Is self.base_store.try_create_optimized() better?
-        match self.base_store.try_create(block_id, data.as_ref()).await? {
-            crate::blockstore::low_level::TryCreateResult::SuccessfullyCreated => Ok(TryCreateResult::SuccessfullyCreated),
-            crate::blockstore::low_level::TryCreateResult::NotCreatedBecauseBlockIdAlreadyExists => Ok(TryCreateResult::NotCreatedBecauseBlockIdAlreadyExists),
+        let lock_guard = self.lock_pool.try_lock(*block_id);
+        match lock_guard {
+            Err(TryLockError::WouldBlock) => {
+                // If the lock is currently held, then the block already exists
+                Ok(TryCreateResult::NotCreatedBecauseBlockIdAlreadyExists)
+            }
+            Err(TryLockError::Poisoned(err)) => {
+                bail!("Log poisoned: {:?}", err)
+            }
+            Ok(_guard) => {
+                // TODO Is self.base_store.try_create_optimized() better?
+                match self.base_store.try_create(block_id, data.as_ref()).await? {
+                    crate::blockstore::low_level::TryCreateResult::SuccessfullyCreated => Ok(TryCreateResult::SuccessfullyCreated),
+                    crate::blockstore::low_level::TryCreateResult::NotCreatedBecauseBlockIdAlreadyExists => Ok(TryCreateResult::NotCreatedBecauseBlockIdAlreadyExists),
+                }
+            }
         }
     }
 
     pub async fn overwrite(&self, block_id: &BlockId, data: &Data) -> Result<()> {
+        let _lock_guard = self.lock_pool.lock_async(*block_id).await;
         // TODO Does an API make more sense where we call a callback similar to with_load, allow call sites to modify that block, and only write back once the callback returns?
         // TODO Is self.base_store.store_optimized() better?
         self.base_store.store(block_id, data).await
     }
 
     pub async fn remove(&self, block_id: &BlockId) -> Result<RemoveResult> {
+        let _lock_guard = self.lock_pool.lock_async(*block_id).await;
+
         match self.base_store.remove(block_id).await? {
             crate::blockstore::low_level::RemoveResult::SuccessfullyRemoved => {
                 Ok(RemoveResult::SuccessfullyRemoved)
