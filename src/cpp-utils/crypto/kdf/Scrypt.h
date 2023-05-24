f74c9196543bc16765b857232949028286a9396c
Sebastian Messmer <messmer@cryfs.org>
2023-05-23 18:32:50 -0700

    Use different values for p and r in scrypt test settings to make sure we catch serialization errors that confuse the two

--- src/cpp-utils/crypto/kdf/Scrypt.h
+++ src/cpp-utils/crypto/kdf/Scrypt.h
@@ -23,7 +23,7 @@ namespace cpputils {
     public:
         static constexpr SCryptSettings ParanoidSettings = SCryptSettings {32, 1048576, 8, 16};
         static constexpr SCryptSettings DefaultSettings = SCryptSettings {32, 1048576, 4, 8};
-        static constexpr SCryptSettings TestSettings = SCryptSettings {32, 1024, 1, 1};
+        static constexpr SCryptSettings TestSettings = SCryptSettings {32, 1024, 1, 2};
 
         explicit SCrypt(const SCryptSettings& settingsForNewKeys);
 
