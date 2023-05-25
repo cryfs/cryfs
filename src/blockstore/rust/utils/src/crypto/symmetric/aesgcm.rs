99271ab017c50c588448c962db07d342a3492902
Sebastian Messmer <messmer@cryfs.org>
2023-05-24 17:58:54 -0700

    TODOs

--- src/blockstore/rust/utils/src/crypto/symmetric/aesgcm.rs
+++ src/blockstore/rust/utils/src/crypto/symmetric/aesgcm.rs
@@ -1,7 +1,8 @@
 use generic_array::typenum::{Unsigned, U12};
 
-// TODO AES-GCM-SIV or XChaCha20-Poly1305 (XChaCha20-Poly1305-ietf, chacha20poly1305_ietf, chacha20poly1305) might be better than AES-GCM
+// TODO AES-GCM-SIV might be better than AES-GCM
 // TODO Add 128bit fixed string to the message and verify it, see https://libsodium.gitbook.io/doc/secret-key_cryptography/aead#robustness
+// TODO Use different keys for different blocks to reduce chance of nonce collisions
 
 use super::CipherDef;
 
@@ -26,7 +27,6 @@ pub type OpensslAes128Gcm =
     super::backends::openssl::AeadCipher<super::backends::openssl::Aes128Gcm<NONCE_SIZE>>;
 pub type AeadAes128Gcm =
     super::backends::aead::AeadCipher<aes_gcm::AesGcm<aes_gcm::aes::Aes128, NONCE_SIZE>>;
-// TODO Libsodium probably has aes128gcm as well, add it.
 
 /// Default aes-128-gcm implementation
 pub type Aes128Gcm = OpensslAes128Gcm;
