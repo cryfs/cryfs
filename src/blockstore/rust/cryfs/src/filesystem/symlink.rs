415a810ba6ef41d0cbf9681b18244ffc25eb1b65
Sebastian Messmer <messmer@cryfs.org>
2023-06-13 21:28:09 -0700

    Implement CrySymlink::target

--- src/blockstore/rust/cryfs/src/filesystem/symlink.rs
+++ src/blockstore/rust/cryfs/src/filesystem/symlink.rs
@@ -1,10 +1,13 @@
 use async_trait::async_trait;
 use std::fmt::Debug;
 
-use super::node::CryNode;
+use super::{
+    fsblobstore::{FsBlob, SymlinkBlob},
+    node::CryNode,
+};
 use cryfs_blobstore::BlobStore;
 use cryfs_rustfs::{object_based_api::Symlink, FsError, FsResult};
-use cryfs_utils::async_drop::AsyncDrop;
+use cryfs_utils::async_drop::{AsyncDrop, AsyncDropGuard};
 
 pub struct CrySymlink<'a, B>
 where
@@ -23,6 +26,17 @@ where
     pub fn new(node: CryNode<'a, B>) -> Self {
         Self { node }
     }
+
+    async fn load_blob(&self) -> FsResult<SymlinkBlob<'a, B>> {
+        let blob = self.node.load_blob().await?;
+        let blob_id = blob.blob_id();
+        FsBlob::into_symlink(blob).await.map_err(|err| {
+            FsError::CorruptedFilesystem {
+                // TODO Add to message what it actually is
+                message: format!("Blob {:?} is listed as a symlink in its parent directory but is actually not a symlink: {err:?}", blob_id),
+            }
+        })
+    }
 }
 
 #[async_trait]
@@ -32,7 +46,12 @@ where
     for<'b> <B as BlobStore>::ConcreteBlob<'b>: Send + Sync,
 {
     async fn target(&self) -> FsResult<String> {
-        // TODO Implement
-        Err(FsError::NotImplemented)
+        let mut blob = self.load_blob().await?;
+        blob.target().await.map_err(|err| {
+            FsError::CorruptedFilesystem {
+                // TODO Add to message what it actually is
+                message: format!("Unparseable symlink blob: {err:?}"),
+            }
+        })
     }
 }
