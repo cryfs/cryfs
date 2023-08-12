0dbe6f0aadbb0e16e9fc91018350ff381145d533
Sebastian Messmer <messmer@cryfs.org>
2023-08-11 23:47:43 -0700

    Implement unlink

--- crates/rustfs/src/object_based_api/low_level_adapter.rs
+++ crates/rustfs/src/object_based_api/low_level_adapter.rs
@@ -295,12 +295,18 @@ where
 
     async fn unlink(
         &self,
-        req: &RequestInfo,
+        _req: &RequestInfo,
         parent_ino: InodeNumber,
         name: &PathComponent,
     ) -> FsResult<()> {
-        // TODO
-        Err(FsError::NotImplemented)
+        let parent = self.get_inode(parent_ino).await?;
+        with_async_drop_2!(parent, {
+            let parent_dir = parent.as_dir().await?;
+            parent_dir
+                .remove_child_file_or_symlink(&name)
+                .await?;
+            Ok(())
+        })
     }
 
     async fn rmdir(
