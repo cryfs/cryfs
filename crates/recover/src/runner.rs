d5d7c7e004d0b2ecfb0b7a338f0c00674c49d2ab
Sebastian Messmer <mail@smessmer.de>
2023-12-04 22:11:14 -1000

    cryfs-recover lists all block ids

--- crates/recover/src/runner.rs
+++ crates/recover/src/runner.rs
@@ -1,8 +1,9 @@
 use anyhow::Result;
 use async_trait::async_trait;
+use futures::stream::TryStreamExt;
 
 use cryfs_blobstore::{BlobId, BlobStoreOnBlocks};
-use cryfs_blockstore::{BlockStore, LockingBlockStore};
+use cryfs_blockstore::{BlockId, BlockStore, LockingBlockStore};
 use cryfs_cli_utils::BlockstoreCallback;
 use cryfs_cryfs::config::ConfigLoadResult;
 use cryfs_utils::async_drop::{AsyncDrop, AsyncDropGuard};
@@ -17,8 +18,23 @@ impl<'l> BlockstoreCallback for RecoverRunner<'l> {
 
     async fn callback<B: BlockStore + AsyncDrop + Send + Sync + 'static>(
         self,
-        blockstore: AsyncDropGuard<LockingBlockStore<B>>,
+        mut blockstore: AsyncDropGuard<LockingBlockStore<B>>,
     ) -> Self::Result {
+        // TODO We should be able to list all blocks in parallel with listing all known blocks
+
+        log::info!("Listing all blocks...");
+        let unaccounted_blocks = match get_all_block_ids(&blockstore).await {
+            Ok(unaccounted_blocks) => unaccounted_blocks,
+            Err(e) => {
+                blockstore.async_drop().await?;
+                return Err(e);
+            }
+        };
+        log::info!(
+            "Listing all blocks...done. Found {} blocks",
+            unaccounted_blocks.len()
+        );
+
         // TODO No unwrap. Should we instead change blocksize_bytes in the config file struct?
         let mut blobstore = BlobStoreOnBlocks::new(
             blockstore,
@@ -38,4 +54,14 @@ impl<'l> BlockstoreCallback for RecoverRunner<'l> {
         blobstore.async_drop().await?;
         Ok(())
     }
+}
+
+async fn get_all_block_ids(
+    blockstore: &AsyncDropGuard<LockingBlockStore<impl BlockStore + Send + Sync>>,
+) -> Result<Vec<BlockId>> {
+    blockstore
+        .all_blocks()
+        .await?
+        .try_collect::<Vec<BlockId>>()
+        .await
 }
