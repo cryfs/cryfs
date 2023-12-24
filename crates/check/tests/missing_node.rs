7968bfd4e9a6ad51b063fd22e77d5f38117967fe
Sebastian Messmer <mail@smessmer.de>
2023-12-24 01:49:09 -1000

    TODOs

--- crates/check/tests/missing_node.rs
+++ crates/check/tests/missing_node.rs
@@ -14,7 +14,12 @@ async fn file_with_missing_leaf_node() {
 }
 
 #[tokio::test(flavor = "multi_thread")]
-async fn file_with_missing_leaf_node_and_inner_node() {
+async fn file_with_missing_inner_node_and_own_leaf_node() {
+    // TODO
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn file_with_missing_inner_node_and_foreign_leaf_node() {
     // TODO
 }
 
@@ -34,7 +39,12 @@ async fn dir_with_missing_leaf_node() {
 }
 
 #[tokio::test(flavor = "multi_thread")]
-async fn dir_with_missing_leaf_node_and_inner_node() {
+async fn dir_with_missing_inner_node_and_own_leaf_node() {
+    // TODO
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn dir_with_missing_inner_node_and_foreign_leaf_node() {
     // TODO
 }
 
@@ -54,6 +64,11 @@ async fn symlink_with_missing_leaf_node() {
 }
 
 #[tokio::test(flavor = "multi_thread")]
-async fn symlink_with_missing_leaf_node_and_inner_node() {
+async fn symlink_with_missing_inner_node_and_own_leaf_node() {
+    // TODO
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn symlink_with_missing_inner_node_and_foreign_leaf_node() {
     // TODO
 }
