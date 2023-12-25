d8b25e5ecddc66e982ac0130a81c50db57aad55e
Sebastian Messmer <mail@smessmer.de>
2023-12-25 08:00:25 -1000

    Add corresponding tests with missing dir/symlink root nodes

--- crates/check/tests/missing_node.rs
+++ crates/check/tests/missing_node.rs
@@ -6,7 +6,9 @@ use cryfs_utils::testutils::asserts::assert_unordered_vec_eq;
 use std::time::SystemTime;
 
 mod common;
-use common::entry_helpers::{add_file_entry, create_dir, create_some_blobs, load_dir_blob};
+use common::entry_helpers::{
+    add_dir_entry, add_file_entry, add_symlink_entry, create_dir, create_some_blobs, load_dir_blob,
+};
 use common::fixture::FilesystemFixture;
 
 fn blobid1() -> BlobId {
@@ -62,7 +64,29 @@ async fn file_with_missing_inner_node_and_foreign_leaf_node() {
 
 #[tokio::test(flavor = "multi_thread")]
 async fn dir_with_missing_root_node() {
-    // TODO
+    let fs_fixture = FilesystemFixture::new().await;
+    let some_blobs = fs_fixture.create_some_blobs().await;
+
+    fs_fixture
+        .update_fsblobstore(|blobstore| async move {
+            let mut parent = load_dir_blob(blobstore, &some_blobs.dir2_dir6).await;
+            // Add a dir entry but don't add the corresponding blob
+            add_dir_entry(&mut parent, "dirname", blobid1());
+            parent.async_drop().await.unwrap();
+        })
+        .await;
+
+    let errors = fs_fixture.run_cryfs_check().await;
+    assert_unordered_vec_eq(
+        vec![
+            // TODO Why is this reported both as a missing blob **and** as a missing node? Can we only report it as a blob? Maybe during the error deduplication step.
+            CorruptedError::BlobMissing { blob_id: blobid1() },
+            CorruptedError::NodeMissing {
+                node_id: *blobid1().to_root_block_id(),
+            },
+        ],
+        errors,
+    );
 }
 
 #[tokio::test(flavor = "multi_thread")]
@@ -87,7 +111,29 @@ async fn dir_with_missing_inner_node_and_foreign_leaf_node() {
 
 #[tokio::test(flavor = "multi_thread")]
 async fn symlink_with_missing_root_node() {
-    // TODO
+    let fs_fixture = FilesystemFixture::new().await;
+    let some_blobs = fs_fixture.create_some_blobs().await;
+
+    fs_fixture
+        .update_fsblobstore(|blobstore| async move {
+            let mut parent = load_dir_blob(blobstore, &some_blobs.root).await;
+            // Add a symlink entry but don't add the corresponding blob
+            add_symlink_entry(&mut parent, "symlinkname", blobid1());
+            parent.async_drop().await.unwrap();
+        })
+        .await;
+
+    let errors = fs_fixture.run_cryfs_check().await;
+    assert_unordered_vec_eq(
+        vec![
+            // TODO Why is this reported both as a missing blob **and** as a missing node? Can we only report it as a blob? Maybe during the error deduplication step.
+            CorruptedError::BlobMissing { blob_id: blobid1() },
+            CorruptedError::NodeMissing {
+                node_id: *blobid1().to_root_block_id(),
+            },
+        ],
+        errors,
+    );
 }
 
 #[tokio::test(flavor = "multi_thread")]
