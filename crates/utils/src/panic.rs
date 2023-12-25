d72452d24b15538de8af935a79d4494deac3a11a
Sebastian Messmer <mail@smessmer.de>
2023-12-25 02:14:07 -1000

    Better reporting of double panics

--- crates/utils/src/panic.rs
+++ crates/utils/src/panic.rs
@@ -1,12 +1,13 @@
 /// A macro that panics but avoids double-panics because they don't show useful error messages.
-/// If the thread is already panicking, it logs an error message instead.
+/// If the thread is already panicking, it prints an error message to stderr instead.
 #[macro_export]
 macro_rules! safe_panic {
     ($($arg:tt)*) => {
         if std::thread::panicking() {
             // We're already panicking, double panic wouldn't show a good error message anyways. Let's just log instead.
             // A common scenario for this to happen is a failing test case.
-            log::error!($($arg)*);
+            eprint!("Panic while already panicking: ");
+            eprintln!($($arg)*);
         } else {
             panic!($($arg)*);
         }
