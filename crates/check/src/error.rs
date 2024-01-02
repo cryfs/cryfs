313bcd1ac6acf479d6b00bd1e22e02f7612a5933
Sebastian Messmer <mail@smessmer.de>
2024-01-02 13:21:08 -1000

    Comment out unused error cases

--- crates/check/src/error.rs
+++ crates/check/src/error.rs
@@ -17,18 +17,16 @@ pub enum CorruptedError {
     #[error("Node {node_id:?} is not referenced but exists")]
     NodeUnreferenced { node_id: BlockId },
 
-    #[error("Node {node_id:?} is referenced but is not reachable. Possibly there is a cycle in a unconnected subtree")]
-    UnreachableSubtreeWithCycle { node_id: BlockId },
-
+    // #[error("Node {node_id:?} is referenced but is not reachable. Possibly there is a cycle in a unconnected subtree")]
+    // UnreachableSubtreeWithCycle { node_id: BlockId },
     #[error("Node {node_id:?} is referenced multiple times")]
     NodeReferencedMultipleTimes { node_id: BlockId },
 
-    #[error("Cyclic self-reference: Node {node_id:?} references itself")]
-    NodeHasCyclicSelfReference { node_id: BlockId },
+    // #[error("Cyclic self-reference: Node {node_id:?} references itself")]
+    // NodeHasCyclicSelfReference { node_id: BlockId },
 
-    #[error("Cyclic self-reference: Dir Blob {blob_id:?} references itself")]
-    DirBlobHasCyclicSelfReference { blob_id: BlobId },
-
+    // #[error("Cyclic self-reference: Dir Blob {blob_id:?} references itself")]
+    // DirBlobHasCyclicSelfReference { blob_id: BlobId },
     #[error("Blob {blob_id:?} is unreadable and likely corrupted")]
     BlobUnreadable {
         blob_id: BlobId,
