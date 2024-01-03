95970e00c3894f4608c3ec4e1bea511133ecd414
Sebastian Messmer <mail@smessmer.de>
2024-01-03 00:53:55 -1000

    Fix reporting of duplicate entries in assert_unordered_vec_eq

--- crates/utils/src/testutils/asserts.rs
+++ crates/utils/src/testutils/asserts.rs
@@ -6,9 +6,7 @@ pub fn assert_unordered_vec_eq<T: Eq + Ord + Debug>(mut lhs: Vec<T>, mut rhs: Ve
     lhs.sort();
     rhs.sort();
     if lhs != rhs {
-        let left_only = lhs.iter().filter(|x| !rhs.contains(x)).collect::<Vec<_>>();
-        let right_only = rhs.iter().filter(|x| !lhs.contains(x)).collect::<Vec<_>>();
-        let both = lhs.iter().filter(|x| rhs.contains(x)).collect::<Vec<_>>();
+        let (left_only, right_only, both) = difference_partition(lhs, rhs);
         panic!(
             "Assertion failed: Vec's are different.\n- both ({both_len}): {both:?}\n- left only ({left_only_len}): {left_only:?}\n- right only ({right_only_len}): {right_only:?}",
             both_len = both.len(),
@@ -16,6 +14,26 @@ pub fn assert_unordered_vec_eq<T: Eq + Ord + Debug>(mut lhs: Vec<T>, mut rhs: Ve
             right_only_len = right_only.len(),
         );
     }
+}
+
+/// Take `lhs` and `rhs` and return three vectors: (`lhs_only`, `rhs_only`, `both`)
+/// with the elements that are only in `lhs`, only in `rhs` or in both.
+fn difference_partition<T: PartialEq + Eq>(
+    mut lhs: Vec<T>,
+    mut rhs: Vec<T>,
+) -> (Vec<T>, Vec<T>, Vec<T>) {
+    let mut both = vec![];
+    lhs.retain(|x| {
+        if let Some(rhs_pos) = rhs.iter().position(|y| *y == *x) {
+            let x = rhs.remove(rhs_pos);
+            both.push(x);
+            false
+        } else {
+            // Keep the entry in `lhs`
+            true
+        }
+    });
+    (lhs, rhs, both)
 }
 
 #[track_caller]
@@ -35,4 +53,103 @@ fn _apply_bound<'a, 'b>(data: &'a [u8], range: &'b impl RangeBounds<usize>) -> &
         Bound::Excluded(&x) => x,
     };
     &data[start..end]
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod difference_partition {
+        use super::*;
+
+        #[test]
+        fn empty() {
+            let (lhs_only, rhs_only, both) = difference_partition::<u8>(vec![], vec![]);
+            assert_eq!(lhs_only, vec![]);
+            assert_eq!(rhs_only, vec![]);
+            assert_eq!(both, vec![]);
+        }
+
+        #[test]
+        fn lhs_only() {
+            let (lhs_only, rhs_only, both) = difference_partition(vec![1, 2, 3], vec![]);
+            assert_eq!(lhs_only, vec![1, 2, 3]);
+            assert_eq!(rhs_only, vec![]);
+            assert_eq!(both, vec![]);
+        }
+
+        #[test]
+        fn rhs_only() {
+            let (lhs_only, rhs_only, both) = difference_partition(vec![], vec![1, 2, 3]);
+            assert_eq!(lhs_only, vec![]);
+            assert_eq!(rhs_only, vec![1, 2, 3]);
+            assert_eq!(both, vec![]);
+        }
+
+        #[test]
+        fn both() {
+            let (lhs_only, rhs_only, both) = difference_partition(vec![1, 2, 3], vec![2, 3, 1]);
+            assert_eq!(lhs_only, vec![]);
+            assert_eq!(rhs_only, vec![]);
+            assert_eq!(both, vec![1, 2, 3]);
+        }
+
+        #[test]
+        fn lhs_and_both() {
+            let (lhs_only, rhs_only, both) = difference_partition(vec![1, 2, 3], vec![2, 3]);
+            assert_eq!(lhs_only, vec![1]);
+            assert_eq!(rhs_only, vec![]);
+            assert_eq!(both, vec![2, 3]);
+        }
+
+        #[test]
+        fn rhs_and_both() {
+            let (lhs_only, rhs_only, both) = difference_partition(vec![1, 2], vec![2, 3, 1]);
+            assert_eq!(lhs_only, vec![]);
+            assert_eq!(rhs_only, vec![3]);
+            assert_eq!(both, vec![1, 2]);
+        }
+
+        #[test]
+        fn lhs_and_rhs() {
+            let (lhs_only, rhs_only, both) = difference_partition(vec![1, 2], vec![3, 4]);
+            assert_eq!(lhs_only, vec![1, 2]);
+            assert_eq!(rhs_only, vec![3, 4]);
+            assert_eq!(both, vec![]);
+        }
+
+        #[test]
+        fn lhs_and_rhs_and_both() {
+            let (lhs_only, rhs_only, both) = difference_partition(vec![1, 2, 3], vec![3, 4, 1]);
+            assert_eq!(lhs_only, vec![2]);
+            assert_eq!(rhs_only, vec![4]);
+            assert_eq!(both, vec![1, 3]);
+        }
+
+        #[test]
+        fn lhs_and_rhs_and_both_and_more() {
+            let (lhs_only, rhs_only, both) =
+                difference_partition(vec![1, 2, 3, 5], vec![3, 4, 1, 6]);
+            assert_eq!(lhs_only, vec![2, 5]);
+            assert_eq!(rhs_only, vec![4, 6]);
+            assert_eq!(both, vec![1, 3]);
+        }
+
+        #[test]
+        fn duplicate_entries() {
+            let (lhs_only, rhs_only, both) =
+                difference_partition(vec![1, 2, 3, 3], vec![3, 1, 4, 1, 6]);
+            assert_eq!(lhs_only, vec![2, 3]);
+            assert_eq!(rhs_only, vec![4, 1, 6]);
+            assert_eq!(both, vec![1, 3]);
+        }
+
+        #[test]
+        fn more_duplicate_entries() {
+            let (lhs_only, rhs_only, both) =
+                difference_partition(vec![1, 2, 2, 3, 3, 3, 3], vec![3, 3, 1, 4, 4, 1, 6]);
+            assert_eq!(lhs_only, vec![2, 2, 3, 3]);
+            assert_eq!(rhs_only, vec![4, 4, 1, 6]);
+            assert_eq!(both, vec![1, 3, 3]);
+        }
+    }
 }
