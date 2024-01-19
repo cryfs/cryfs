11bda95e8bdeec57659d50c70d13d78bc44e310e
Sebastian Messmer <messmer@cryfs.org>
2024-01-18 22:41:02 -0800

    - Make Error type in task_queue configurable - Add test case for tasks throwing errors

--- crates/check/src/task_queue.rs
+++ crates/check/src/task_queue.rs
@@ -1,4 +1,3 @@
-use anyhow::Result;
 use futures::future::{BoxFuture, FutureExt};
 use futures::stream::{StreamExt, TryStreamExt};
 use std::future::Future;
@@ -7,13 +6,15 @@ use tokio_stream::wrappers::UnboundedReceiverStream;
 
 /// Call this with an `initial_task` and it will call that task, and all tasks recursively spawned by it, until all tasks are done.
 /// It will restrict the number of tasks running concurrently to `max_concurrency`.
-pub async fn run_to_completion<'f, F>(
+pub async fn run_to_completion<'f, F, E>(
     max_concurrency: usize,
-    initial_task: impl FnOnce(TaskSpawner<'f>) -> F,
-) -> Result<()>
+    initial_task: impl FnOnce(TaskSpawner<'f, E>) -> F,
+) -> Result<(), E>
 where
-    F: Future<Output = Result<()>> + Send + 'f,
+    F: Future<Output = Result<(), E>> + Send + 'f,
 {
+    assert!(max_concurrency > 0, "Cannot run with max_concurrency == 0");
+
     let (sender, receiver) = unbounded_channel();
     TaskSpawner { sender }.spawn(initial_task);
 
@@ -24,14 +25,14 @@ where
     Ok(())
 }
 
-pub struct TaskSpawner<'f> {
-    sender: UnboundedSender<BoxFuture<'f, Result<()>>>,
+pub struct TaskSpawner<'f, E = anyhow::Error> {
+    sender: UnboundedSender<BoxFuture<'f, Result<(), E>>>,
 }
 
-impl<'f> TaskSpawner<'f> {
+impl<'f, E> TaskSpawner<'f, E> {
     pub fn spawn<F>(&self, future: impl FnOnce(Self) -> F)
     where
-        F: Future<Output = Result<()>> + Send + 'f,
+        F: Future<Output = Result<(), E>> + Send + 'f,
     {
         let future = future(TaskSpawner {
             sender: self.sender.clone(),
@@ -44,15 +45,14 @@ impl<'f> TaskSpawner<'f> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::sync::atomic::{AtomicUsize, Ordering};
     use std::sync::Arc;
 
     #[tokio::test]
     async fn spawn_100_tasks_directly() {
-        use super::*;
-        use std::sync::atomic::{AtomicUsize, Ordering};
         let counter = Arc::new(AtomicUsize::new(0));
         let counter_clone = Arc::clone(&counter);
-        run_to_completion(10, |spawner| async move {
+        run_to_completion(10, |spawner: TaskSpawner<'_, ()>| async move {
             for _ in 0..100 {
                 let counter_clone = Arc::clone(&counter_clone);
                 spawner.spawn(move |_| async move {
@@ -69,16 +69,14 @@ mod tests {
 
     #[tokio::test]
     async fn spawn_100_tasks_recursively() {
-        use super::*;
-        use std::sync::atomic::{AtomicUsize, Ordering};
         let counter = Arc::new(AtomicUsize::new(0));
 
         #[async_recursion::async_recursion]
         async fn task(
-            spawner: TaskSpawner<'static>,
+            spawner: TaskSpawner<'static, ()>,
             counter: Arc<AtomicUsize>,
             index: usize,
-        ) -> Result<()> {
+        ) -> Result<(), ()> {
             counter.fetch_add(1, Ordering::SeqCst);
             if index < 100 {
                 spawner
@@ -91,5 +89,29 @@ mod tests {
             .await
             .unwrap();
         assert_eq!(100, counter.load(Ordering::SeqCst));
+    }
+
+    #[tokio::test]
+    async fn spawn_error_task() {
+        let counter = Arc::new(AtomicUsize::new(0));
+
+        #[async_recursion::async_recursion]
+        async fn task(
+            spawner: TaskSpawner<'static, &'static str>,
+            counter: Arc<AtomicUsize>,
+            index: usize,
+        ) -> Result<(), &'static str> {
+            counter.fetch_add(1, Ordering::SeqCst);
+            if index < 100 {
+                spawner
+                    .spawn(move |spawner| async move { task(spawner, counter, index + 1).await });
+                Ok(())
+            } else {
+                Err("error message")
+            }
+        }
+
+        let result = run_to_completion(10, |spawner| task(spawner, Arc::clone(&counter), 1)).await;
+        assert_eq!(Err("error message"), result);
     }
 }
