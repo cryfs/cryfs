86c569731e61018584cd56fae99d7088350c3436
Sebastian Messmer <mail@smessmer.de>
2024-02-08 00:36:32 -1000

    Now that bad blob ids don't panic anymore but throw an error, we can add a test to cryfs-check for it

--- crates/check/tests/blob_unreadable.rs
+++ crates/check/tests/blob_unreadable.rs
@@ -36,31 +36,27 @@ async fn unreadable_blob_bad_format_version(#[case] blob_id: impl FnOnce(&SomeBl
     assert_unordered_vec_eq(expected_errors, errors);
 }
 
-// TODO
-// #[rstest]
-// #[case::file(|some_blobs: &SomeBlobs| some_blobs.large_file_1)]
-// #[case::dir(|some_blobs: &SomeBlobs| some_blobs.large_dir_1)]
-// #[case::symlink(|some_blobs: &SomeBlobs| some_blobs.large_symlink_1)]
-// #[case::rootdir(|some_blobs: &SomeBlobs| some_blobs.root)]
-// #[tokio::test(flavor = "multi_thread")]
-// async fn unreadable_file_blob_bad_blob_type(#[case] blob_id: impl FnOnce(&SomeBlobs) -> BlobId) {
-//     let (fs_fixture, some_blobs) = FilesystemFixture::new_with_some_blobs().await;
-//     let blob_id = blob_id(&some_blobs);
-//     let orphaned_descendant_blobs = fs_fixture.get_descendants_if_dir_blob(blob_id).await;
+#[rstest]
+#[case::file(|some_blobs: &SomeBlobs| some_blobs.large_file_1)]
+#[case::dir(|some_blobs: &SomeBlobs| some_blobs.large_dir_1)]
+#[case::symlink(|some_blobs: &SomeBlobs| some_blobs.large_symlink_1)]
+#[case::rootdir(|some_blobs: &SomeBlobs| some_blobs.root)]
+#[tokio::test(flavor = "multi_thread")]
+async fn unreadable_file_blob_bad_blob_type(#[case] blob_id: impl FnOnce(&SomeBlobs) -> BlobId) {
+    let (fs_fixture, some_blobs) = FilesystemFixture::new_with_some_blobs().await;
+    let blob_id = blob_id(&some_blobs);
+    let orphaned_descendant_blobs = fs_fixture.get_descendants_if_dir_blob(blob_id).await;
 
-//     fs_fixture.corrupt_blob_type(some_blobs.large_file_1).await;
+    fs_fixture.corrupt_blob_type(blob_id).await;
 
-//     let expected_errors =
-//         iter::once(CorruptedError::BlobUnreadable {
-//             blob_id: some_blobs.large_file_1,
-//         })
-//         .chain(orphaned_descendant_blobs.into_iter().map(|child| {
-//             CorruptedError::NodeUnreferenced {
-//                 node_id: *child.to_root_block_id(),
-//             }
-//         }))
-//         .collect();
+    let expected_errors = iter::once(CorruptedError::BlobUnreadable { blob_id })
+        .chain(orphaned_descendant_blobs.into_iter().map(|child| {
+            CorruptedError::NodeUnreferenced {
+                node_id: *child.to_root_block_id(),
+            }
+        }))
+        .collect();
 
-//     let errors = fs_fixture.run_cryfs_check().await;
-//     assert_unordered_vec_eq(expected_errors, errors);
-// }
+    let errors = fs_fixture.run_cryfs_check().await;
+    assert_unordered_vec_eq(expected_errors, errors);
+}
