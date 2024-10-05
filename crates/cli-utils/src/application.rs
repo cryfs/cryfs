c6dd07c7cf2411fdffdb99a09222c79c943c6af0
Sebastian Messmer <messmer@cryfs.org>
2024-10-04 22:39:15 -0700

    Allow the `RUST_BACKTRACE` environment variable to overwrite our custom panic handler

--- crates/cli-utils/src/application.rs
+++ crates/cli-utils/src/application.rs
@@ -34,20 +34,29 @@ pub fn run<App: Application>() -> Result<()> {
 }
 
 fn show_backtrace_on_panic<App: Application>() {
-    if cfg!(debug_assertions) {
-        // In debug builds, always show a backtrace on panic, irrespective of the `RUST_BACKTRACE` environment variable
-        std::panic::set_hook(Box::new(|panic_info| {
-            let backtrace = std::backtrace::Backtrace::force_capture();
-            eprintln!("{panic_info}");
-            eprintln!("\nBacktrace:\n{backtrace}");
-        }));
-    } else {
-        // In release builds, show a human readable error message and generate a dump file for the user to upload with the issue report
-        human_panic::setup_panic!(
-            human_panic::Metadata::new(App::NAME, App::VERSION.to_string())
-                .authors(env!("CARGO_PKG_AUTHORS").replace(":", ", "))
-                .homepage(env!("CARGO_PKG_HOMEPAGE"))
-                .support("Open a ticket at https://github.com/cryfs/cryfs/issues and include the report file.")
-        );
+    match ::std::env::var("RUST_BACKTRACE") {
+        Ok(_) => {
+            // The `RUST_BACKTRACE` environment variable is set, change nothing and just use the default behavior of that variable.
+        }
+        Err(_) => {
+            // The `RUST_BACKTRACE` environment variable is not set, define our own default behavior
+            if cfg!(debug_assertions) {
+                // In debug builds, always show a backtrace on panic, irrespective of the `RUST_BACKTRACE` environment variable
+                std::panic::set_hook(Box::new(|panic_info| {
+                    let backtrace = std::backtrace::Backtrace::force_capture();
+                    eprintln!("{panic_info}");
+                    eprintln!("\nBacktrace:\n{backtrace}");
+                }));
+            } else {
+                // In release builds, show a human readable error message and generate a dump file for the user to upload with the issue report
+                human_panic::setup_panic!(
+                    human_panic::Metadata::new(App::NAME, App::VERSION.to_string())
+                        .authors(env!("CARGO_PKG_AUTHORS").replace(":", ", "))
+                        .homepage(env!("CARGO_PKG_HOMEPAGE"))
+                        .support("Open a ticket at https://github.com/cryfs/cryfs/issues and include the report file.")
+                );
+                // TODO https://github.com/rust-cli/human-panic/issues/155
+            }
+        }
     }
 }
