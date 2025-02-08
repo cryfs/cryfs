08c7d81eb2a09d0da1d9e3bfbb4f4de727be02db
Sebastian Messmer <messmer@cryfs.org>
2025-02-08 10:43:49 -0800

    Add validation for --ciphers argument

--- crates/cryfs-cli/src/args.rs
+++ crates/cryfs-cli/src/args.rs
@@ -76,7 +76,7 @@ pub struct MountArgs {
     /// If creating a new file system, this will determine the cipher for the new file system.
     /// If opening an existing file system, this will verify that the file system actually uses that cipher.
     /// See possible values by calling cryfs with --show-ciphers. Default: xchacha20-poly1305
-    #[arg(long)]
+    #[arg(long,value_parser=clap::builder::PossibleValuesParser::new(cryfs_filesystem::config::ALL_CIPHERS))]
     pub cipher: Option<String>, // TODO This should probably be an enum
 
     // TODO Make display of default blocksize dynamic to show the actual default blocksize
