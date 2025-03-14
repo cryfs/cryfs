11b69f28855da9de4b8d14c09f315d32ca69ee50
Sebastian Messmer <messmer@cryfs.org>
2025-03-13 18:05:12 -0700

    read and write correctly update atime/mtime

--- crates/cryfs-filesystem/src/filesystem/open_file.rs
+++ crates/cryfs-filesystem/src/filesystem/open_file.rs
@@ -75,6 +75,32 @@ where
         }
         Ok(())
     }
+
+    async fn _read(&self, offset: NumBytes, size: NumBytes) -> FsResult<Data> {
+        let mut blob = self.load_blob().await?;
+        // TODO Is it better to have try_read return a Data object instead of a &mut [u8]? Or should we instead make OpenFile::read() take a &mut [u8]?
+        //      The current way of mapping between the two ways of doing it in here is probably not optimal.
+        let mut data: Data = vec![0; u64::from(size) as usize].into();
+        // TODO Push down the NumBytes type and use it in blobstore/blockstore interfaces?
+        let num_read_bytes = blob
+            .try_read(&mut data, offset.into())
+            .await
+            .map_err(|err| {
+                log::error!("Failed to read from blob: {err:?}");
+                FsError::UnknownError
+            })?;
+        data.shrink_to_subregion(..num_read_bytes);
+        Ok(data)
+    }
+
+    async fn _write(&self, offset: NumBytes, data: Data) -> FsResult<()> {
+        let mut blob = self.load_blob().await?;
+        // TODO Push down the NumBytes type and use it in blobstore/blockstore interfaces?
+        blob.write(&data, offset.into()).await.map_err(|err| {
+            log::error!("Failed to write to blob: {err:?}");
+            FsError::UnknownError
+        })
+    }
 }
 
 impl<B> Debug for CryOpenFile<B>
@@ -115,29 +141,31 @@ where
     }
 
     async fn read(&self, offset: NumBytes, size: NumBytes) -> FsResult<Data> {
-        let mut blob = self.load_blob().await?;
-        // TODO Is it better to have try_read return a Data object instead of a &mut [u8]? Or should we instead make OpenFile::read() take a &mut [u8]?
-        //      The current way of mapping between the two ways of doing it in here is probably not optimal.
-        let mut data: Data = vec![0; u64::from(size) as usize].into();
-        // TODO Push down the NumBytes type and use it in blobstore/blockstore interfaces?
-        let num_read_bytes = blob
-            .try_read(&mut data, offset.into())
-            .await
-            .map_err(|err| {
-                log::error!("Failed to read from blob: {err:?}");
-                FsError::UnknownError
-            })?;
-        data.shrink_to_subregion(..num_read_bytes);
-        Ok(data)
+        // read should update atime if (and only if) size > 0, see https://pubs.opengroup.org/onlinepubs/9699919799/functions/read.html
+        let should_update_atime = size > NumBytes::from(0);
+        if should_update_atime {
+            self.node_info
+                .concurrently_maybe_update_access_timestamp_in_parent(&self.blobstore, async || {
+                    self._read(offset, size).await
+                })
+                .await
+        } else {
+            self._read(offset, size).await
+        }
     }
 
     async fn write(&self, offset: NumBytes, data: Data) -> FsResult<()> {
-        let mut blob = self.load_blob().await?;
-        // TODO Push down the NumBytes type and use it in blobstore/blockstore interfaces?
-        blob.write(&data, offset.into()).await.map_err(|err| {
-            log::error!("Failed to write to blob: {err:?}");
-            FsError::UnknownError
-        })
+        // write should update mtime if (and only if) size > 0, see https://pubs.opengroup.org/onlinepubs/9699919799/functions/write.html
+        let should_update_mtime = data.len() > 0;
+        if should_update_mtime {
+            self.node_info
+                .concurrently_update_modification_timestamp_in_parent(&self.blobstore, async || {
+                    self._write(offset, data).await
+                })
+                .await
+        } else {
+            self._write(offset, data).await
+        }
     }
 
     async fn flush(&self) -> FsResult<()> {
