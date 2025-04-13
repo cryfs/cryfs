926822a4e3778dac7362637b80aca584d217c753
Michael Cho <michael@michaelcho.dev>
2025-04-13 10:37:29 -0400

    Fix building with Boost 1.88

--- src/cpp-utils/process/subprocess.cpp
+++ src/cpp-utils/process/subprocess.cpp
@@ -1,7 +1,18 @@
 #include "subprocess.h"
 #include <array>
 #include <boost/asio.hpp>
+#include <boost/version.hpp>
+#if BOOST_VERSION < 108800
 #include <boost/process.hpp>
+#else
+#define BOOST_PROCESS_VERSION 1
+#include <boost/process/v1/args.hpp>
+#include <boost/process/v1/async_pipe.hpp>
+#include <boost/process/v1/child.hpp>
+#include <boost/process/v1/exe.hpp>
+#include <boost/process/v1/io.hpp>
+#include <boost/process/v1/search_path.hpp>
+#endif
 #include <cerrno>
 #include <cstddef>
 #include <cstdio>
