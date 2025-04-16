8f538933c68429d3aed27f865af56f8bf5eae8f0
Sebastian Messmer <messmer@cryfs.org>
2025-04-16 15:08:20 -0700

    TODOs

--- crates/cryfs-cli/src/lib.rs
+++ crates/cryfs-cli/src/lib.rs
@@ -15,3 +15,5 @@ cryfs_version::assert_cargo_version_equals_git_version!();
 
 // TODO Throughout the codebase, remove the `as` keyword and replace with `from`/`try_from`
 // TODO Throughout the codebase, check where it makes sense to replace usize/u64 with byte_unit::Byte
+
+// TODO Unhelpful error message when things go wrong (e.g. basedir exists but doesn't have a cryfs.config). Improve them.
