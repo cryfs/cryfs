598b485f0e5887743ccd0d8b99d2aff82fa37fd4
Sebastian Messmer <messmer@cryfs.org>
2025-05-04 23:45:52 -0700

    TODOs

--- crates/e2e-perf-tests/src/operations/mod.rs
+++ crates/e2e-perf-tests/src/operations/mod.rs
@@ -6,5 +6,5 @@ mod lookup;
 mod mkdir;
 
 // TODO Somehow none of the operations have different counts based on the atime update behavior? That seems odd, shouldn't the atime update behavior affect the number of operations needed?
-// TODO Test other operations
+// TODO Test other operations: setattr/fsetattr (chmod/fchmod, chown/fchown, chgrp/fchgrp, truncate/ftruncate, utimens, futimens), readlink, unlink, rmdir, symlink, rename, open, read, write, flush, fsync, readdir, statfs
 // TODO Go through all the operation counts in the tests and think about whether we can reduce the number of needed operations
