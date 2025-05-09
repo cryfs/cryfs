f6ed527fca84b4ceb5c3dcab42113790fb280b56
Sebastian Messmer <messmer@cryfs.org>
2025-05-09 16:15:24 -0700

    TODOs

--- crates/e2e-perf-tests/src/operations/mod.rs
+++ crates/e2e-perf-tests/src/operations/mod.rs
@@ -11,6 +11,6 @@ mod statfs;
 mod symlink;
 
 // TODO Somehow none of the operations have different counts based on the atime update behavior? That seems odd, shouldn't the atime update behavior affect the number of operations needed?
-//      Operations that should be affected: mkdir, create_file, symlink (they need to update the parent dir's timestamp in the grandparent dir), readdir, rename, rmdir, read, write, unlink, others? readlink does change based on atime somehow.
+//      Operations that should be affected: mkdir, create_file, symlink (they need to update the parent dir's timestamp in the grandparent dir), readdir, rename, rmdir, read, write, unlink, others? readdir and readlink do change based on atime somehow. But correctly?
 // TODO Test other operations: setattr/fsetattr (chmod/fchmod, chown/fchown, chgrp/fchgrp, truncate/ftruncate, utimens, futimens), unlink, rmdir, rename, read, write, flush, fsync
 // TODO Go through all the operation counts in the tests and think about whether we can reduce the number of needed operations
