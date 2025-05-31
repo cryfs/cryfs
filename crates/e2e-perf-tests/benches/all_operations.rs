626c33714552cbd4c62d1be49af7812b8a3310b7
Sebastian Messmer <messmer@cryfs.org>
2025-05-30 18:58:30 -0700

    Remove dummy benchmark

--- crates/e2e-perf-tests/benches/all_operations.rs
+++ crates/e2e-perf-tests/benches/all_operations.rs
@@ -1,11 +1,5 @@
 use criterion::{Criterion, criterion_group, criterion_main};
 
-pub fn dummy_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| {}));
-}
-
-criterion_group!(dummy_benches, dummy_benchmark);
-
 #[cfg(not(feature = "benchmark"))]
 fn main() {
     panic!(
