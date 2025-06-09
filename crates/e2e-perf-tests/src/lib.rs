c0e0bb417164d1d037a00bd44ecbf433673980d9
Sebastian Messmer <messmer@cryfs.org>
2025-06-09 04:47:54 -0700

    Write short README for e2e-perf-tests

--- crates/e2e-perf-tests/src/lib.rs
+++ crates/e2e-perf-tests/src/lib.rs
@@ -1,5 +1,30 @@
-//! This crate contains end-to-end tests for cryfs, testing the number of blobs and blocks that are read or written by each file system operation.
+//! This crate contains performance tests and benchmarks for CryFS.
+//!
+//! # Performance tests
+//! These tests run filesystem operations and check the number of blobs and blocks that are read or written by each file system operation.
 //! This is meant to ensure that we don't accidentally run more operations than necessary and don't regress performance over time.
+//!
+//! To run these, just run
+//! $ cargo test
+//!
+//! # Benchmarks
+//! This crate also allows running these tests as [criterion] benchmarks, measuring the time they take.
+//!
+//! To run these, just run
+//! $ cargo bench --feature benchmark
+//!
+//! The `benchmark` feature is necessary so we generate benchmark code instead of test code.
+//!
+//! # Implementation details
+//! Performance tests are pretty fast and are
+//! - implemented on top of an [InMemoryBlockStore]
+//! - executed by directly calling the [rustfs] filesystem API. No need to mount it or use fuse
+//!
+//! Benchmarks aim to be more realistic and are
+//! - implemented on top of a [TempDirBlockStore] (i.e. blocks are stored on the real file system)
+//! - executed by mounting CryFS using [fuse] or [fuse_mt] and executing filesystem operations through real OS syscalls
+
+#![cfg(any(test, feature = "benchmark"))]
 
 mod filesystem_driver;
 mod filesystem_fixture;
@@ -7,7 +32,6 @@ pub mod operations;
 pub mod perf_test_macro;
 mod test_driver;
 
-// TODO Write README
 // TODO Improve behavior when "benchmarking" feature is on/off, e.g. fix unused code warnings.
 // TODO Fix all warnings
 // TODO Ensure CI is running tests and building the benchmarks (but probably doesn't need to run it)
