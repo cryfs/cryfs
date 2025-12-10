9a267527789c87e5cd2a52216e0263b5c512e9ab
Sebastian Messmer <messmer@cryfs.org>
2025-12-10 10:19:41 -0800

    Remove unused export

--- crates/blobstore/src/lib.rs
+++ crates/blobstore/src/lib.rs
@@ -22,9 +22,4 @@ pub use cryfs_blockstore::RemoveResult;
 #[cfg(test)]
 mod tests;
 
-// This is needed by rstest_reuse, at least in 0.5.0, because otherwise they can't find their macros
-#[cfg(test)]
-#[cfg(feature = "slow-tests-any")]
-use rstest_reuse;
-
 cryfs_version::assert_cargo_version_equals_git_version!();
